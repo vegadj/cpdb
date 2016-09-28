17369c290ca239c68331e5371a140a15568adcab
Gokhan SELAMET <gokhan@gair.local>
2016-09-28 22:27:02 +0300

    Initial Commit

---
+++ cpdb/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  cpdb
+//
+//  Created by Gokhan SELAMET on 28/09/2016.
+//  Copyright © 2016 Gokhan SELAMET. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
